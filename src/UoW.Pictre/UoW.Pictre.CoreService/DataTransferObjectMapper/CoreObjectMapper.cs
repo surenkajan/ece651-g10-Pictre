33d8d294fc8e9bb6b5cb4a001a48ac81824794c7
etom123 <31858448+etom123@users.noreply.github.com>
2017-11-12 17:18:57 -0500

    latest changes

--- src/UoW.Pictre/UoW.Pictre.CoreService/DataTransferObjectMapper/CoreObjectMapper.cs
+++ src/UoW.Pictre/UoW.Pictre.CoreService/DataTransferObjectMapper/CoreObjectMapper.cs
@@ -25,7 +25,42 @@ namespace UoW.Pictre.CoreService.DataTransferObjectMapper
 
         }
 
-      
+      public static UserDto UserDaoToDto(User userDao)
+        {
+            if (userDao == null) return null;
+            UserDto dto = new UserDto();
+            dto.FirstName = userDao.FirstName;
+            dto.LastName = userDao.LastName;
+            dto.FullName = userDao.FullName;
+            dto.EmailAddress = userDao.EmailAddress;
+            dto.DateOfBirth = userDao.DateOfBirth;
+            dto.Sex = userDao.Sex;
+
+            return dto;
+
+        }
+
+        public static User UserDtoToDao(UserDto userDto)
+        {
+            if (userDto == null) return null;
+            return new User()
+            {
+                FirstName = userDto.FirstName,
+                LastName = userDto.LastName,
+                FullName = userDto.FullName,
+                EmailAddress = userDto.EmailAddress,
+                DateOfBirth = userDto.DateOfBirth,
+                Sex = userDto.Sex
+            };
+        }
+
+        public static List<UserDto> UserDaoToDto(List<User> userDaoList)
+        {
+            if (userDaoList == null) return null;
+            var userList = (from userObj in userDaoList
+                           select UserDaoToDto(userObj)).ToList();
+            return userList;
+        }
 
         public static FriendDto FriendDaoToDto(Friend friend)
         {
