05488cd1691488d8cffe0123370184dbb46421b2
SHITIJ-PC\SHITIJ <svasist@uwaterloo.ca>
2017-11-11 23:34:24 -0500

    sample c sharp rest request

--- src/UoW.Pictre/UoW.Pictre.Web.WebForms/FriendMap/FriendMap.aspx.cs
+++ src/UoW.Pictre/UoW.Pictre.Web.WebForms/FriendMap/FriendMap.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,15 @@ namespace UoW.Pictre.Web.WebForms.FriendMap
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            var client = new RestClient("http://localhost:32785/Service.svc/userrest/GetUserByEmailID?Email=brindha@gmail.com");
 
+            IRestResponse response = client.Execute(new RestRequest());
+
+            JObject json = JObject.Parse(response.Content);
+
+            String FirstName = Convert.ToString(json["GetUserByEmailIDResult"]["FirstName"]);
+
+            System.Diagnostics.Debug.WriteLine(FirstName);
         }
     }
 }
