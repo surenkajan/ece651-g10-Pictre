d7c6aa344f841050ad58184dbf181f55ee659d4c
etom123 <31858448+etom123@users.noreply.github.com>
2017-11-12 02:31:26 -0500

    To get profile pic and first name of friends

---
+++ src/UoW.Pictre/PictreDB/SchemaObjects/Schemas/pictre/Programmability/StoredProcedures/CoreGetFriendByEmailID.proc.sql
@@ -0,0 +1,45 @@
+USE [Pictre]
+GO
+
+IF EXISTS ( SELECT * 
+            FROM   sysobjects 
+            WHERE  id = object_id(N'[pictre].[CoreGetFriendByEmailID]') 
+                   and OBJECTPROPERTY(id, N'IsProcedure') = 1 )
+BEGIN
+    DROP PROCEDURE [pictre].[CoreGetFriendByEmailID]
+END
+GO
+
+
+CREATE PROCEDURE [pictre].[CoreGetFriendByEmailID] @EmailAddress VARCHAR(240)
+As
+Begin
+ DECLARE @temp TABLE
+    (
+    FirstName varchar(30),
+    ProfilePhoto varchar(30)
+    
+    )
+ DECLARE 
+ @name VARCHAR(50)
+	
+
+ DECLARE db_cursor CURSOR FOR  
+SELECT FriendID 
+FROM [pictre].[Friends] 
+WHERE ID  IN (select ID from [pictre].[User] where EmailAddress =@EmailAddress)  
+
+OPEN db_cursor   
+FETCH NEXT FROM db_cursor INTO @name   
+
+WHILE @@FETCH_STATUS = 0   
+BEGIN   
+   insert into @temp   select FirstName,ProfilePhoto from [pictre].[User] where ID=  @name
+
+       FETCH NEXT FROM db_cursor INTO @name   
+END   
+select * from @temp ;
+CLOSE db_cursor   
+DEALLOCATE db_cursor
+END
+GO
