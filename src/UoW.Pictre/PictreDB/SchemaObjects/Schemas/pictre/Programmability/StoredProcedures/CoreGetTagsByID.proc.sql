c8113d83c9cf9570716ed7417e7e47902f0556ba
etom <etom@uwaterloo.ca>
2017-11-14 19:35:06 -0500

    Added procedure for get Tags by ID

---
+++ src/UoW.Pictre/PictreDB/SchemaObjects/Schemas/pictre/Programmability/StoredProcedures/CoreGetTagsByID.proc.sql
@@ -0,0 +1,25 @@
+﻿USE [Pictre]
+GO
+
+IF EXISTS ( SELECT * 
+            FROM   sysobjects 
+            WHERE  id = object_id(N'[pictre].[CoreGetTagsByID]') 
+                   and OBJECTPROPERTY(id, N'IsProcedure') = 1 )
+BEGIN
+    DROP PROCEDURE [pictre].[CoreGetTagsByID]
+END
+GO
+
+
+CREATE PROCEDURE [pictre].[CoreGetTagsByID] @PhotoID int
+As
+Begin
+
+
+	
+	select b.FirstName,b.LastName from  (select UserID from [pictre].[Tags] where PhotoID = @PhotoID) a,
+	(select FirstName,ID,LastName from [pictre].[User])  b
+	 where a.UserID=b.ID 
+   
+END
+GO
