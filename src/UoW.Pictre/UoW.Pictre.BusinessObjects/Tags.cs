a5f1abd3887868afcb0d960bf08061b21cad1243
Jaspreet Singh Sambee <jaspreet.sambee@gmail.com>
2017-11-16 10:52:59 -0800

    Prliminary Tag commit

---
+++ src/UoW.Pictre/UoW.Pictre.BusinessObjects/Tags.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UoW.Pictre.BusinessObjects
+{
+    public class Tags : BusinessObject
+    {
+        #region Database Properties
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+
+        #endregion
+    }
+}
