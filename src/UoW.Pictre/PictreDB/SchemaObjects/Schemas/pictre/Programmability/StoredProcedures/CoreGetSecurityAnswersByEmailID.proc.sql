fdad27b1e559d1d837ca3d78086e7f0b403afedc
Kajaruban Surendran <surenkajan@gmail.com>
2017-11-20 19:22:32 -0500

    Stored Procedure is created to get Security Answers

---
+++ src/UoW.Pictre/PictreDB/SchemaObjects/Schemas/pictre/Programmability/StoredProcedures/CoreGetSecurityAnswersByEmailID.proc.sql
@@ -0,0 +1,30 @@
+﻿USE [Pictre]
+GO
+
+IF EXISTS ( SELECT * 
+            FROM   sysobjects 
+            WHERE  id = object_id(N'[pictre].[GetCoreSecurityAnswersEmailID]') 
+                   and OBJECTPROPERTY(id, N'IsProcedure') = 1 )
+BEGIN
+    DROP PROCEDURE [pictre].[GetCoreSecurityAnswersEmailID]
+END
+GO
+
+
+CREATE PROCEDURE [pictre].[GetCoreSecurityAnswersEmailID]
+	@EmailAddress			VARCHAR(240),
+	@Question			VARCHAR(max)
+
+AS
+	-- Answers to the 1st Questions
+	SELECT * FROM [pictre].[UserSecurity] 
+	WHERE 
+	UserID = (SELECT ID FROM [Pictre].[pictre].[User] WHERE EmailAddress = @EmailAddress) 
+	AND 
+	QuestionID = (SELECT [ID] FROM [Pictre].[pictre].[SecurityQuestion] WHERE Question = @Question)
+
+	GO
+	
+
+	
+	
